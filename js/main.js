38172350bd1b40667c0d1b5125eb3f33f6c64b86
Salvatore Santagati <salvatore.santagati@gmail.com>
2018-07-16 12:08:06 +0200

    feat: add api key

--- js/main.js
+++ js/main.js
@@ -95,7 +95,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: 'myAPI',
+    mapboxToken: 'pk.eyJ1Ijoic2Fsc2FuIiwiYSI6ImNqamZ3ODYyNzM0NDIza256bnNhOG5zY3MifQ.G9fDCj_p3dkobRkBzhb3fA',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
