5c337f37875a438467871dc5eac1faeba972fabd
Salvatore Santagati <salvatore.santagati@gmail.com>
2018-07-16 10:29:32 +0200

    fix: resolve problem with a11y

--- js/main.js
+++ js/main.js
@@ -177,8 +177,8 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
-  image.alt = restaurant.name;
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = restaurant.name + " Restaurant";
   li.append(image);
 
   const name = document.createElement('h1');
